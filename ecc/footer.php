a3b3f983c19621b3bf6c33f5c9feb9888a86b1d0
Mark Erickson <mark.erickson@webfilings.com>
2012-05-19 15:07:21 -0500

    Added address, phone, email address, service times to template footer.

--- ecc/footer.php
+++ ecc/footer.php
@@ -3,13 +3,20 @@
 
 <div id="footer">
 
-<p class="copyright">Copyright &copy; Evangelical Covenant Church. All rights reserved.</p>
+<p class="copyright">Evangelical Covenant Church<br/>&copy; eccdsm.org.  All Rights Reserved.</p>
+
+<p class="info">2625 Hubbell Avenue</br>Des Moines, IA  50317<br/>515.266.1732 - <script>document.write('<'+'a'+' '+'h'+'r'+'e'+'f'+'='+"'"+'m'+'a'+'i'+'l'+'t'+'o'+'&'+'#'+'5'+'8'+';'+'o'+'f'+'&'+'#'+'1'+
+'0'+'2'+';'+'i'+'&'+'#'+'3'+'7'+';'+'6'+'3'+'%'+'6'+'5'+'&'+'#'+'6'+'4'+';'+'e'+'&'+'#'+'9'+'9'+';'+
+'c'+'%'+'6'+'4'+'%'+'7'+'3'+'m'+'&'+'#'+'4'+'6'+';'+'%'+'6'+'F'+'r'+'g'+"'"+'>'+'o'+'&'+'#'+'1'+'0'+
+'2'+';'+'f'+'i'+'c'+'&'+'#'+'1'+'0'+'1'+';'+'&'+'#'+'6'+'4'+';'+'&'+'#'+'1'+'0'+'1'+';'+'c'+'c'+'&'+
+'#'+'1'+'0'+'0'+';'+'s'+'m'+'&'+'#'+'4'+'6'+';'+'&'+'#'+'1'+'1'+'1'+';'+'r'+'&'+'#'+'1'+'0'+'3'+';'+
+'<'+'/'+'a'+'>');</script></p>
+
+<p class="info">Sunday Services</br>Blended – 9:00 a.m.<br />Contemporary – 10:45 a.m.</p>
 
 <ul id="socialIcons">
-	<li class="connect">Stay connected on Facebook, Twitter, Youtube &amp; RSS feed:</li>
     <li><a href="http://www.facebook.com/eccdsm"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/socialIcons/FaceBook.png" alt="Facebook" name="Facebook" width="27" height="27" border="0" id="Facebook" /></a></li>
     <li><a href="http://twitter.com/ECCDSM"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/socialIcons/Twitter.png" alt="Twitter" name="Twitter" width="27" height="27" border="0" id="Twitter" /></a></li>
-    <li><a href="http://www.youtube.com/user/ECCDSM"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/socialIcons/Youtube.png" alt="YouTube Channel" name="YouTube" width="27" height="27" border="0" id="YouTube" /></a></li>
     <li><a href="<?php bloginfo('rss2_url'); ?>"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/socialIcons/RSSFeed.png" alt="RSS Feed" name="RSS" width="26" height="26" border="0" id="RSS" /></a></li>
 </ul>
     
